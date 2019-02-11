8a4056b5e6538b96c29da86f927cef2702c16458
huahaitao <huahaitao739@pingan.com.cn>
2019-02-11 23:00:59 +0800

    add search support

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="h-doc-footer" :style="style">
-    <div class="h-doc-footer-bottom">Made with <span class="h-doc-footer-heart">❤</span> by Billow</div>
-  </div>
+  <footer class="h-doc-footer" :style="style">
+    <div class="container">
+      <div class="footer-main">
+        <h4>title</h4>
+        <a href="https://github.com" class="footer-main-link" target="_blank">repo</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">changelog</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">faq</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">starter</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">theme</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">preview</a>
+      </div>
+      <div class="footer-main">
+        <h4>title</h4>
+        <a href="gitterLink" class="footer-main-link" target="_blank">gitter</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">feedback</a>
+        <a href="`https://github.com" class="footer-main-link" target="_blank">contribution</a>
+        <a href="https://segmentfault.com" class="footer-main-link" target="_blank">SegmentFault</a>
+        <a href="https://github.com" class="footer-main-link" target="_blank">Awesome</a>
+      </div>
+      <div class="footer-social">
+        <i class="doc-icon-weixin" v-popover:weixin></i>
+        <a href="https://github.com" target="_blank">
+          <i class="doc-icon-github "></i>
+        </a>
+        <a href="gitterLink" target="_blank">
+          <i class="doc-icon-gitter"></i>
+        </a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
--- src/component/Header.vue
+++ src/component/Header.vue
@@ -6,6 +6,9 @@
           <span>{{ config.logo.title }}</span>
         </a>
         <ul class="h-doc-header__top-nav">
+          <li>
+            <input id="search-form" v-model="query" @input="querySearch">
+          </li>
           <li
             v-for="(value, key) in config.nav"
             class="h-doc-header__top-nav-item"
@@ -30,7 +33,10 @@
                 version="1.1"
                 aria-hidden="true"
               >
-                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                ></path>
               </svg>
               <span v-else>{{ key }}</span>
             </a>
@@ -42,17 +48,46 @@
 </template>
 
 <script>
+import algoliasearch from "algoliasearch";
 export default {
-  name: 'h-doc-header',
+  name: "h-doc-header",
   props: {
     config: Object,
     active: String
+  },
+  data() {
+    return {
+      index: null,
+      query: ""
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      const client = algoliasearch(
+        "B4S7CGPIRI",
+        "3cd68460584895f4b86269124b475b38"
+      );
+      this.index = client.initIndex("contact");
+    },
+    querySearch() {
+      if (!this.query) return;
+      this.index.search({ query: this.query, hitsPerPage: 6 }, (err, res) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      console.log(res);
+      });
+    }
   }
 };
 </script>
 
 <style lang="less">
-@import '../style/var.less';
+@import "../style/var.less";
 
 .h-doc-header {
   width: 100%;
@@ -102,7 +137,7 @@ export default {
         color: @h-doc-text-color;
       }
       .h-doc-header__arrow::after {
-        content: '';
+        content: "";
         display: inline-block;
         vertical-align: middle;
         margin-top: -1px;
@@ -126,7 +161,8 @@ export default {
     span {
       font-size: 22px;
       color: @h-doc-black;
-      font-family: 'Dosis', 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
+      font-family: "Dosis", "Source Sans Pro", "Helvetica Neue", Arial,
+        sans-serif;
     }
   }
   &__bottom {
